94f5f72dd8730cbc44424430e293021ad57cc0cc
MAGNAPRO <info@magnapro.eu>
2022-10-17 16:02:29 +0200

    Update wallpaper_url.ps1

--- Delfin/DuckyScript/Wallpaper_From_WWW/wallpaper_url.ps1
+++ Delfin/DuckyScript/Wallpaper_From_WWW/wallpaper_url.ps1
@@ -1,4 +1,4 @@
-$url = "https://github.com/magnaproeu/githubTest/blob/7268a2e4bd62758962a5e9ecce950a9cdf042042/Delfin/DuckyScript/Wallpaper_From_WWW/wall_01.jpg"
+$url = "https://github.com/magnaproeu/githubTest/Delfin/DuckyScript/Wallpaper_From_WWW/wall_01.jpg"
 
 $ext = $url.split(".")[-1]
 
